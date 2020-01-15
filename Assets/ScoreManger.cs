a38168de3c1fa19842492cc504da9e57aaac2afb
youhei sato <s.y.92.jr@gmail.com>
2020-01-16 02:27:37 +0900

    Lesson5課題提出
    得点内容(小さい星：10点、大きい星：100点、小さな雲：25点、大きな雲：50点)

---
+++ Assets/ScoreManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger : MonoBehaviour {
+
+	
+	public GameObject score_object = null;
+	public int score_num;
+
+	void Start () {
+		Initialize ();
+	}
+
+	void Update () {
+		
+		Text score_text = score_object.GetComponent<Text> ();
+
+		score_text.text = "SCORE:" + score_num;
+	}
+    public void AddPoint (int point)
+    {
+        score_num =  score_num + point;
+    }
+
+    private void Initialize ()
+    {
+    	score_num = 0;
+    }
+}
